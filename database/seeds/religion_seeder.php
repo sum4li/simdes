f4b49124e5f7a6fde7281fcba52d3429790505ab
sum4li <shofie.gg@gmail.com>
2019-05-23 06:03:22 +0700

    make seeder for religion

---
+++ database/seeds/religion_seeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class religion_seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $name = ['Islam','Kristen','Katholik','Hindu','Budha'];
+
+
+        for($i=0;$i<count($name);$i++){
+            App\Product::create([
+                'name'=> $name[$i],
+                'slug'=> str_slug($name[$i])
+            ]);
+        }
+    }
+}
