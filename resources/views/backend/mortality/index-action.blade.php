6efb86bff2541f6a695403f5ade001385016f36c
sum4li <shofie.gg@gmail.com>
2019-05-31 14:08:24 +0700

    add module kematian / mortality

---
+++ resources/views/backend/mortality/index-action.blade.php
@@ -0,0 +1,21 @@
+<a href="{{route('mortality.show',[$id])}}"
+    class="btn btn-info btn-sm shadow-sm"
+    data-toggle="tooltip"
+    data-placement="top"
+    title="Detail">
+    <i class="fa fa-eye"></i>
+</a>
+<a href="{{route('mortality.edit',[$id])}}"
+    class="btn btn-success btn-sm shadow-sm"
+    data-toggle="tooltip"
+    data-placement="top"
+    title="Edit">
+    <i class="fa fa-pen"></i>
+</a>
+<a href="{{route('mortality.destroy',[$id])}}"
+    class="btn btn-danger btn-sm shadow-sm delete-data"
+    data-toggle="tooltip"
+    data-placement="top"
+    title="Delete">
+    <i class="fa fa-times"></i>
+</a>
