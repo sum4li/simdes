5af7414aca5a8751aee12d4e35ddb4737a45077b
sum4li <shofie.gg@gmail.com>
2019-05-31 14:08:05 +0700

    membuat routes api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Transformers\ReligionTransformer;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,20 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+header('Access-Control-Allow-Origin:  *');
+// header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+// header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/religion', function(){
+    return fractal(App\Religion::all(),new ReligionTransformer)->respond();
+
+
+});
+
+Route::post('/lul',function(){
+    return 'lol';
 });
