c25b7869ae746d660a3f3ed482e06368719060fe
sum4li <shofie.gg@gmail.com>
2019-05-31 14:07:31 +0700

    edit application name

--- resources/views/backend/component/footer.blade.php
+++ resources/views/backend/component/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>2019. Made by <a href="http://digsa.id">Digsa.id</a> with <i class="fa fa-heart text-danger"></i></span>
+            <span>2019. Made with <i class="fa fa-heart text-danger"></i> by <a href="http://digsa.id" target="_blank">Digsa.id</a> </span>
             {{-- <span>Copyright {{date('Y')}} digsa.id. All Right Reserved</span> --}}
         </div>
     </div>
--- resources/views/backend/component/sidebar.blade.php
+++ resources/views/backend/component/sidebar.blade.php
@@ -9,7 +9,7 @@
     <!-- Divider -->
     <hr class="sidebar-divider">
     <div class="sidebar-heading">
-        Menu Utama
+        Menu
     </div>
     <!-- Nav Item - Dashboard -->
     <li class="nav-item {{active('dashboard')}}">
@@ -19,13 +19,21 @@
         </a>
     </li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Addons
-    </div>
-    <li class="nav-item {{active('religion.index')}}">
+
+    <li class="nav-item">
+        <a class="nav-link {{is_active(['religion.*','hamlet.*','civil.*']) ? '':'collapsed'}}" href="#" data-toggle="collapse" data-target="#master_data" aria-expanded="true" aria-controls="master_data">
+            <i class="fas fa-fw fa-book"></i>
+            <span>Master Data</span>
+        </a>
+        <div id="master_data" class="collapse {{is_active(['religion.*','hamlet.*','civil.*'])  ? 'show':''}}" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item {{active('religion.*')}}" href="{{route('religion.index')}}">Agama</a>
+            <a class="collapse-item {{active('hamlet.*')}}" href="{{route('hamlet.index')}}">Dukuh / Dusun</a>
+            <a class="collapse-item {{active('civil.*')}}" href="{{route('civil.index')}}">Penduduk</a>
+            </div>
+        </div>
+    </li>
+    {{-- <li class="nav-item {{active('religion.index')}}">
         <a class="nav-link" href="{{route('religion.index')}}">
             <i class="fas fa-fw fa-boxes"></i>
             <span>Agama</span>
@@ -36,21 +44,21 @@
             <i class="fas fa-fw fa-boxes"></i>
             <span>Dukuh</span>
         </a>
-    </li>
-    <li class="nav-item {{active('civil.index')}}">
+    </li> --}}
+    {{-- <li class="nav-item {{active('civil.index')}}">
         <a class="nav-link" href="{{route('civil.index')}}">
             <i class="fas fa-fw fa-users"></i>
             <span>Penduduk</span>
         </a>
-    </li>
+    </li> --}}
     <li class="nav-item">
         <a class="nav-link {{is_active('transaction.*') ? '':'collapsed'}}" href="#" data-toggle="collapse" data-target="#transaksi" aria-expanded="true" aria-controls="transaksi">
             <i class="fas fa-fw fa-book"></i>
-            <span>Transaksi</span>
+            <span>Kejadian</span>
         </a>
-        <div id="transaksi" class="collapse {{is_active('transaction.*')  ? 'show':''}}" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div id="transaksi" class="collapse {{is_active(['mortality.*'])  ? 'show':''}}" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-            <a class="collapse-item {{active('transaction.create')}}" href="{{route('transaction.create')}}">Transaksi</a>
+            <a class="collapse-item {{active('mortality.*')}}" href="{{route('mortality.index')}}">Kematian</a>
             <a class="collapse-item {{active('transaction.index')}}" href="{{route('transaction.index')}}">List Transaksi</a>
             <a class="collapse-item {{active('transaction.history')}}" href="{{route('transaction.history')}}">Riwayat Transaksi</a>
             </div>
@@ -62,6 +70,8 @@
             <span>Setting</span>
         </a>
     </li>
+    @if (auth()->user()->role->name == 'super-admin')
+
     <li class="nav-item">
         <a class="nav-link {{is_active('user.index') || is_active('role.index') ? '':'collapsed'}}" href="#" data-toggle="collapse" data-target="#user" aria-expanded="true" aria-controls="user">
             <i class="fas fa-fw fa-user"></i>
@@ -74,6 +84,7 @@
             </div>
         </div>
     </li>
+    @endif
     {{-- <li class="nav-item {{active('user.index')}}">
         <a class="nav-link" href="{{route('user.index')}}">
             <i class="fas fa-fw fa-users"></i>
